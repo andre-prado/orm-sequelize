51b0929329fae78d55c28cd63c10c27973c46a50
andre-prado <andr3prad0@gmail.com>
2022-01-16 22:50:43 -0300

    Cria CRUD para Pessoas

--- api/controllers/PessoasController.js
+++ api/controllers/PessoasController.js
@@ -5,7 +5,63 @@ class PessoaController {
     try {
       const todasAsPessoas = await database.Pessoas.findAll();
       return res.status(200).json(todasAsPessoas);
-    } catch (error) {
+    } 
+    catch (error) {
+      return res.status(500).json(error.message);
+    }
+  }
+
+  static async pegaUmaPessoa(req, res) {
+    try {
+      const { id } = req.params;
+
+      const umaPessoa = await database.Pessoas.findOne({where: {id: id}});
+      return res.status(200).json(umaPessoa);
+    } 
+    catch (error) {
+      return res.status(500).json(error.message);
+    }
+  }
+
+  static async criaUmaPessoa(req, res) {
+    const novaPessoa = req.body;
+    try {
+      const novaPessoaCriada = await database.Pessoas.create(novaPessoa);
+      return res.status(201).json(novaPessoaCriada);
+    }
+    catch(err) {
+      return res.status(500).json(error.message);
+    }
+  }
+
+  static async atualizaUmaPessoa(req, res) {
+    const novosDados = req.body;
+    const { id } = req.params;
+
+    try {
+      await database.Pessoas.update(
+      novosDados,
+      {
+        where:
+        {
+          id: id
+        }
+      });
+      return res.status(204).json();
+    }
+    catch(err) {
+      return res.status(500).json(error.message);
+    }
+  }
+
+  static async deletaUmaPessoa(req, res) {
+    const { id } = req.params;
+
+    try {
+      await database.Pessoas.destroy({where: {id: id }});
+      return res.status(202).json();
+    }
+    catch(err) {
       return res.status(500).json(error.message);
     }
   }
--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -4,5 +4,11 @@ const PessoaController = require('../controllers/PessoasController');
 const router = Router();
 
 router.get('/pessoas', PessoaController.pegaTodasAsPessoas);
+router.get('/pessoas/:id', PessoaController.pegaUmaPessoa);
+router.post('/pessoas', PessoaController.criaUmaPessoa);
+router.put('/pessoas/:id', PessoaController.atualizaUmaPessoa);
+router.delete('/pessoas/:id', PessoaController.deletaUmaPessoa);
+
+
 
 module.exports = router;
